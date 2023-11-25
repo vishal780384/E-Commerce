1e9539cb41b5d7e7a796519227388294bb4638bf
Vishal Kushwaha <124510425+vishal780384@users.noreply.github.com>
2023-11-25 14:51:13 +0530

    Hero.jsx

--- frontend/src/Components/Hero/Hero.jsx
+++ frontend/src/Components/Hero/Hero.jsx
@@ -1,32 +1,32 @@
-
-import React, { useState } from 'react';
-import './Hero.css'
-import img1 from  '../Assests/banner_mens.png'
+import React, { useState, useEffect } from 'react';
+import img1 from '../Assests/banner_mens.png'
 import img2 from '../Assests/banner_women.png'
-import img3 from '../Assests/banner_kids.png' 
-import img4 from '../Assests/Try some New.png' 
-
+import img3 from '../Assests/banner_kids.png'
+import './Hero.css'
 
 const ImageCarousel = () => {
-  const images = [img1, img2, img3,img4];
+  const images = [img1, img2, img3];
   const [currentImage, setCurrentImage] = useState(0);
 
   const nextImage = () => {
     setCurrentImage((currentImage + 1) % images.length);
   };
 
-  const prevImage = () => {
-    setCurrentImage((currentImage - 1 + images.length) % images.length);
-  };
+  // for making image previous
+
+  // const prevImage = () => {
+  //   setCurrentImage((currentImage - 1 + images.length) % images.length);   
+  // };
+
+  useEffect(() => {
+    const interval = setInterval(() => { nextImage(); }, 2000); // Change the interval time here (in milliseconds)
+    return () => clearInterval(interval);
+  }, [currentImage]);
 
   return (
     <div className="image-carousel">
       <div className="carousel-container">
-        <img src={images[currentImage]} alt=" " width="400" height="300" />
-      </div>
-      <div className="controls">
-        <button onClick={prevImage}>Previous</button>
-        <button onClick={nextImage}>Next</button>
+        <img src={images[currentImage]} alt="" width="400" height="300" />
       </div>
     </div>
   );
